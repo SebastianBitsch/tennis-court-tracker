29c016b85f06221525c726e606185c11beed36e3
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2024-01-11 21:46:12 +0100

    assorted changes, nothing major

--- tennis_court_tracker/train_model.py
+++ tennis_court_tracker/train_model.py
@@ -4,52 +4,54 @@ import hydra
 import logging
 
 from omegaconf import DictConfig
-
+from hydra.utils import get_original_cwd, to_absolute_path
 from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
 
-from tennis_court_tracker.data.TennisCourtDataset import TennisCourtDataset, Rescale, ToTensor
+from tennis_court_tracker.data.TennisCourtDataset import TennisCourtDataset#, Rescale, ToTensor
 from tennis_court_tracker.models.model import TrackNet
 
+logger = logging.getLogger(__name__)
 
-@hydra.main(config_path="conf", config_name="config")
+
+@hydra.main(version_base="1.2", config_path="conf", config_name="config")
 def train(config: DictConfig) -> None:
     device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 
     court_dataset = TennisCourtDataset(
-        "data/processed/keypoints.csv", 
-        "data/processed/images",
-        "data/processed/labels",
+        annotations_file_path = to_absolute_path("data/raw/data_train.json"), 
+        images_dir = to_absolute_path("data/raw/images"),
+        device = device,
         transform = transforms.Compose([
-            Rescale((360, 640)),
-            ToTensor(device)
+            transforms.Resize((config.data.image_height, config.data.image_width), antialias=True)
+            # transforms.RandomCrop((config.data.image_height, config.data.image_width), antialias=True)
         ])
     )
 
-    train_dataset, validation_dataset = random_split(court_dataset, lengths=(0.8, 0.2))
-
+    train_dataset, validation_dataset = random_split(court_dataset, lengths = (config.data.pct_train_split, 1.0 - config.data.pct_train_split))
+    
     train_dataloader = DataLoader(train_dataset, batch_size=config.hyperparameters.batch_size, shuffle=True, num_workers=0)
     validation_dataloader = DataLoader(validation_dataset, batch_size=config.hyperparameters.batch_size, shuffle=False, num_workers=0)
 
-
-    model = TrackNet(in_features = 3).to(device)
+    model = TrackNet(in_features = config.data.n_in_features).to(device)
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adadelta(model.parameters(), lr = config.hyperparameters.learning_rate)
 
     wandb.init(
-        project = "tennis-court-tracking",
+        project = config.wandb.project_name,
         config = {
-            "learning_rate": config.hyperparameters.learning_rate,
             "architecture": "CNN",
             "dataset": "Custom-1",
+            "learning_rate": config.hyperparameters.learning_rate,
             "epochs": config.hyperparameters.epochs,
             "batch_size" : config.hyperparameters.batch_size,
         },
-        mode = config.wandb.mode # disable for testing
+        mode = config.wandb.mode
     )
 
+
     for epoch in range(config.hyperparameters.epochs):
-        print(f"\n---- Epoch {epoch+1}/{config.hyperparameters.epochs} ----")
+        logger.info(f"**** Epoch {epoch+1}/{config.hyperparameters.epochs} ****")
 
         total_loss = 0.0
         validation_loss = 0.0
@@ -58,9 +60,9 @@ def train(config: DictConfig) -> None:
         model.train()
         for batch_num, batch in enumerate(train_dataloader):
             x = batch['image']
-            y = batch['label']
+            y = batch['heatmap']
 
-            y_pred = model(x) # shape: [batch_size, 256, 360, 640]
+            y_pred = model(x) # shape: [batch_size, 256, image_height, image_width]
 
             loss = loss_fn(y_pred, y)
             loss.backward()
@@ -69,36 +71,39 @@ def train(config: DictConfig) -> None:
             optimizer.zero_grad()
 
             total_loss += loss.item()
-            if (batch_num % 10 == 0):
-                print(f"{batch_num}/{len(train_dataloader)} | loss: {loss:.2f}")
+            if (batch_num % 1 == 0):
+                logger.info(f"{batch_num + 1}/{len(train_dataloader)} | loss: {loss:.3f}")
                 wandb.log({"loss": loss})
-    
+            break
 
         # Validate
         model.eval()
         with torch.no_grad():
             for batch_num, batch in enumerate(validation_dataloader):
                 x = batch['image']
-                y = batch['label']
+                y = batch['heatmap']
 
                 y_pred = model(x)
                 loss = loss_fn(y_pred, y)
                 validation_loss += loss.item()
+                logger.info(f"{batch_num + 1}/{len(train_dataloader)} | val loss: {loss.item():.3f}")
 
                 # Log a sample image
                 id = 0
                 if batch_num == id:
                     im_true = y[id]
                     im_pred = y_pred[id].argmax(dim=0)
-                    im_stack = torch.vstack([im_true, im_pred]).cpu().float()
+                    im_stack = torch.hstack([im_true, im_pred]).cpu().float()
                     wandb.log({"sample_image": wandb.Image(im_stack, caption="Top: Input, Bottom: Output")})
 
+
         wandb.log({"train_loss": total_loss, "validation_loss" : validation_loss})
-        print(f"Train loss: {total_loss:.2f},   | Val loss: {validation_loss:.2f}")
+        logger.info(f"Train loss: {total_loss:.3f},   | Val loss: {validation_loss:.3f}")
         torch.save(model.state_dict(), "models/model16.pt")
 
     wandb.finish()
     torch.save(model.state_dict(), "models/final_model16.pt")
+
 
 if __name__ == "__main__":
     train()
