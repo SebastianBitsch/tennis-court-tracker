86c46c46dba7d887f4cb4eafc95d88964b61ee5d
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2024-01-11 17:21:50 +0100

    assorted notebooks

--- tennis_court_tracker/predict_model.py
+++ tennis_court_tracker/predict_model.py
@@ -1,4 +1,15 @@
 import torch
+import wandb
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+
+from tennis_court_tracker.data.TennisCourtDataset import TennisCourtDataset, Rescale, ToTensor
+from tennis_court_tracker.models.model import TrackNet
+
 
 def predict(
     model: torch.nn.Module,
@@ -14,4 +25,34 @@ def predict(
         Tensor of shape [N, d] where N is the number of samples and d is the output dimension of the model
 
     """
-    return torch.cat([model(batch) for batch in dataloader], 0)
+    model.eval()
+    return torch.cat([model(batch['image']) for batch in dataloader], 0)
+
+
+if __name__ == "__main__":
+
+    # Load model
+    model_path = "model.pt"
+    model = TrackNet(in_features = 3)
+    model.load_state_dict(torch.load(model_path))
+
+    dataset = TennisCourtDataset(
+        "data/processed/keypoints.csv", 
+        "data/processed/images",
+        "data/processed/labels",
+        transform = transforms.Compose([
+            Rescale((360, 640)),
+            ToTensor()
+        ])
+    )
+    n = 1
+    dataset_subset = torch.utils.data.Subset(dataset, range(n))
+    dataloader = DataLoader(dataset_subset, batch_size=1, shuffle=False, num_workers=0)
+
+    predictions = predict(model, dataloader)
+    pred = predictions[0].squeeze().argmax(dim=0).detach().numpy()
+    print(np.sum(0 < pred))
+    plt.imshow(pred)
+    plt.show()
+
+
