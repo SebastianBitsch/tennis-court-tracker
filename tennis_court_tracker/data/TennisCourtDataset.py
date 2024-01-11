470bdd644e3b2a242f8b2fbe64896ed8ac1c4953
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2024-01-11 21:45:43 +0100

    new dataset model to accomodate coco data

--- tennis_court_tracker/data/TennisCourtDataset.py
+++ tennis_court_tracker/data/TennisCourtDataset.py
@@ -1,137 +1,66 @@
-import os
+import json
+
 import torch
-import pandas as pd
-import numpy as np
+
 from torch.utils.data import Dataset
-from skimage.transform import resize
+from torchvision.io import read_image
+
 
 class TennisCourtDataset(Dataset):
-    """Tennis court dataset, adapted from: https://pytorch.org/tutorials/beginner/data_loading_tutorial.html """
+    """ Tennis court dataset, adapted from: https://pytorch.org/tutorials/beginner/data_loading_tutorial.html """
 
-    def __init__(self, keypoints_file:str, images_dir:str, labels_dir:str, transform=None) -> None:
-        """
-        Arguments:
-            keypoints_file (string): Path to the csv file with annotations.
-            images_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
-        """
-        self.keypoints_df = pd.read_csv(keypoints_file)
+    def __init__(self, annotations_file_path:str, images_dir:str, device:str = 'cpu', transform = None) -> None:
+        """  """
         self.images_dir = images_dir
-        self.labels_dir = labels_dir
         self.transform = transform
+        self.device = device
+        with open(annotations_file_path) as f:
+            self.annotations = json.load(f)
 
-    def __len__(self):
-        return len(self.keypoints_df)
+    def __len__(self) -> int:
+        return len(self.annotations)
 
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        img_name = os.path.join(self.images_dir, self.keypoints_df.iloc[idx]['image_name'])
-        image = np.load(img_name)
-
-        label_name = os.path.join(self.labels_dir, self.keypoints_df.iloc[idx]['image_name'])
-        label = np.load(label_name)
-
-        # keypoints = self.keypoints_df.iloc[idx, 2:]
-        # keypoints = np.array([keypoints], dtype=float).reshape(-1, 2)
+    def __getitem__(self, idx:int) -> dict:
+        """ Get the next image and heatmap """
+        image_id = self.annotations[idx]['id']
+        image_path = f"{self.images_dir}/{image_id}.png"
         
-        # sample = {'image': image, 'keypoints': keypoints}
-        sample = {'image': image, 'label': label}
+        image = read_image(image_path)
+        heatmap = generate_heatmap(image.shape[1:], self.annotations[idx]['kps'])
 
         if self.transform:
-            sample = self.transform(sample)
-
-        return sample
-    
-
-class Rescale(object):
-    """Rescale the image in a sample to a given size.
-
-    Args:
-        output_size (tuple or int): Desired output size. If tuple, output is
-            matched to output_size. If int, smaller of image edges is matched
-            to output_size keeping aspect ratio the same.
-    """
-
-    def __init__(self, output_size):
-        assert isinstance(output_size, (int, tuple))
-        self.output_size = output_size
-
-    def __call__(self, sample):
-        image, label = sample['image'], sample['label']
-
-        h, w = image.shape[:2]
-        if isinstance(self.output_size, int):
-            if h > w:
-                new_h, new_w = self.output_size * h / w, self.output_size
-            else:
-                new_h, new_w = self.output_size, self.output_size * w / h
-        else:
-            new_h, new_w = self.output_size
-
-        new_h, new_w = int(new_h), int(new_w)
-
-        # TODO: This is pretty temporary
-        img = resize(image, (new_h, new_w, 3)).astype("float32")
-        label = resize(label, (new_h, new_w, 1)).astype("float32")
-
-        # h and w are swapped for keypoints because for images,
-        # x and y axes are axis 1 and 0 respectively
-        # keypoints = keypoints * [new_w / w, new_h / h]
-
-        return {'image': img, 'label': label}
-
-
-class RandomCrop(object):
-    """Crop randomly the image in a sample.
-
-    Args:
-        output_size (tuple or int): Desired output size. If int, square crop
-            is made.
-    """
-
-    def __init__(self, output_size):
-        assert isinstance(output_size, (int, tuple))
-        if isinstance(output_size, int):
-            self.output_size = (output_size, output_size)
-        else:
-            assert len(output_size) == 2
-            self.output_size = output_size
-
-    def __call__(self, sample):
-        image, keypoints = sample['image'], sample['keypoints']
-
-        h, w = image.shape[:2]
-        new_h, new_w = self.output_size
-
-        top = np.random.randint(0, h - new_h + 1)
-        left = np.random.randint(0, w - new_w + 1)
-
-        image = image[top: top + new_h, left: left + new_w]
-
-        keypoints = keypoints - [left, top]
-
-        return {'image': image, 'keypoints': keypoints}
-
-
-class ToTensor(object):
-    """Convert ndarrays in sample to Tensors."""
-
-    def __init__(self, device:str = "cpu") -> None:
-        self.device = device
-
-    def __call__(self, sample):
-        image, label = sample['image'], sample['label']
-
-        # swap color axis because
-        # numpy image: H x W x C
-        # torch image: C x H x W
-        image = image.transpose((2, 0, 1))
-        label = label.transpose((2, 0, 1))
+            image = self.transform(image)
+            heatmap = self.transform(heatmap)
 
         return {
-            'image': torch.tensor(image, device = self.device),
-            'label': torch.tensor(label, device = self.device, dtype=torch.uint8).squeeze() # This has to be long for crossentropyloss to work on the cpu, for some reason - look into that uint8 works on mps
+            "image" : image.float().to(self.device), 
+            "heatmap" : heatmap.squeeze().to(self.device)
         }
+
+
+def gaussian_kernel(size:int, sigma2:int):
+    """Generates a Gaussian kernel. Centered at the middle"""
+    x = torch.arange(-size // 2 + 1., size // 2 + 1.)
+    x = (1.0 / (2 * torch.pi * sigma2)) * torch.exp(-(((x - 0)** 2 + (x - 0)**2) / (2 * sigma2))) * (2 * torch.pi * sigma2)
+    kernel = torch.outer(x, x)
+    return kernel
+
+def point_in_image(image_size: tuple[int,int], point: tuple[int,int], border_size:int):
+    return 0 <= point[0] - border_size and 0 <= point[1] - border_size and point[0] + border_size < image_size[0] and point[1] + border_size < image_size[1]
+
+def generate_heatmap(size: tuple[int,int], keypoints:list, radius:int = 5, sigma2:int = 10) -> torch.FloatTensor:
+    """
+    Generate a "heatmap" of points on an image. 
+    Every point will be represented by a gaussian on the image
+    returns a uint8 array of values in range 0-255
+    not all keypoints are sure to be in the image, especially true when image is cropped etc
+    """
+    heatmap = torch.zeros((1, *size), dtype=torch.uint8)
+    gaussian = (255 * gaussian_kernel(2 * radius, sigma2)).to(torch.uint8)
+    
+    for cx, cy in keypoints:
+        if not point_in_image(size, (cy, cx), radius):
+            continue
+        heatmap[0, cy-radius:cy+radius, cx-radius:cx+radius] = gaussian
+
+    return heatmap
