fef7c4fb81ba5fa39ae9b958a0c40a772bcf5fa2
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2024-01-27 11:30:49 +0100

    better point detection

--- tennis_court_tracker/models/postprocess.py
+++ tennis_court_tracker/models/postprocess.py
@@ -6,6 +6,11 @@ from torchvision import transforms as TF
 from tennis_court_tracker.models.tracknet import TrackNet
 
 from scipy.optimize import linear_sum_assignment
+from tennis_court_tracker.data.court import TENNISCOURT
+
+from skimage.morphology import erosion
+from skimage.morphology import disk 
+
 
 class PredictPoints(nn.Module):
     """ """
@@ -17,37 +22,50 @@ class PredictPoints(nn.Module):
         self.pipeline = nn.Sequential(
             TrackNet(in_features = in_features, out_features=1, weights_path=weights_path), # TODO: out_features might need to change
             TF.ConvertImageDtype(torch.uint8),
-            nn.Threshold(130, 0), # TODO: Threshold should maybe be dynamic (?)
-            TF.GaussianBlur(kernel_size = (9,9), sigma = 2)
+            nn.Threshold(254, 0), # TODO: Threshold should maybe be dynamic (?)
         ).to(device)
+
+
+    def _detect_points(self, binary_image: np.ndarray) -> np.ndarray:
+        """ """
+        # We have to invert the image, so dumb - but doesnt seem to work without
+        inv_binary = cv2.bitwise_not(binary_image)
+
+        params = cv2.SimpleBlobDetector_Params()         
+        # TODO: We can filter out points on all sorts of parameters. See: https://stackoverflow.com/a/28573944/19877091
+
+        detector = cv2.SimpleBlobDetector_create(params) 
+        pred_points = cv2.KeyPoint_convert(detector.detect(inv_binary))
+
+        return pred_points
+
 
     def forward(self, im: torch.Tensor) -> list:
 
         with torch.no_grad():
-            pred_heatmaps = self.pipeline(im).squeeze(1) # dim: [batchsize, out_features, h, w]
+            pred_heatmaps = self.pipeline(im).squeeze(1) # dim: [batchsize, out_features, h, w] -> [batchsize, h, w]
 
-        # TODO: Iterate over the batch. I dont think this can be parallelized nicely
         predicted_points = []
+        # TODO: Iterate over the batch. I dont think this can be parallelized nicely
         for heatmap in pred_heatmaps.cpu().numpy():
-            pred_points = cv2.HoughCircles(
-                heatmap,
-                cv2.HOUGH_GRADIENT,
-                dp=1,           # Inverse ratio of accumulator resolution to image resolution
-                minDist=20,     # Minimum distance between the centers of the detected circles
-                param1=10,      # Upper threshold for the edge detector
-                param2=3,       # Threshold for center detection
-                minRadius=1,    # Minimum radius of the detected circles
-                maxRadius=20    # Maximum radius of the detected circles
-            )
-            predicted_points.append(pred_points.squeeze()[:, :2])
-        
+            
+            for erosion_amount in range(5):
+                binary_im = erosion(heatmap, disk(erosion_amount))
+                pred_points = self._detect_points(binary_im)
+                
+                # We want to keep removing points until we have a reasonable amount to get rid of noise
+                if len(pred_points) < 1.5 * len(TENNISCOURT.points):
+                    print(f"Found binary image with {len(pred_points)} points at erosion({erosion_amount})")
+                    predicted_points.append(pred_points)
+                    break
+
         return predicted_points
 
 
 class FindHomography(torch.nn.Module):
     """ """
     def __init__(self) -> None:
-        """ NOTE: This doesnt work for batched input, expects just a single set of points. Not really a torch module"""
+        """ NOTE: This doesnt work for batched input, expects just a single set of points. Not really a torch module just pretending to be"""
         super(FindHomography, self).__init__()
 
     def normalize_points(self, points: np.ndarray) -> np.ndarray:
@@ -68,6 +86,6 @@ class FindHomography(torch.nn.Module):
             court_points[court_idxs, :], 
             method=cv2.RANSAC, 
             ransacReprojThreshold=5.0, 
-            maxIters=10000
+            maxIters=1000000
         )
         return H, pred_idxs
